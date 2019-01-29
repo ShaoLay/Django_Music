51ba1cb4c0aca1d1a112301fcc1216d9ce88d9e3
hamdi <javs_shao@163.com>
2019-01-29 17:06:05 +0800

    模型类_歌曲信息表song

--- music/index/models.py
+++ music/index/models.py
@@ -15,3 +15,23 @@ class Label(models.Model):
         verbose_name = '歌曲分类'
         verbose_name_plural = '歌曲分类'
 
+# 歌曲信息表song
+class Song(models.Model):
+    song_id = models.AutoField('序号', primary_key=True)
+    song_name = models.CharField('歌名', max_length=50)
+    song_singer = models.CharField('歌手', max_length=50)
+    song_time = models.CharField('时长', max_length=10)
+    song_album = models.CharField('专辑', max_length=50)
+    song_languages = models.CharField('语种', max_length=20)
+    song_type = models.CharField('类型', max_length=20)
+    song_release = models.CharField('发行时间', max_length=20)
+    song_img = models.CharField('歌曲图片', max_length=20)
+    song_lyrics = models.CharField('歌词', max_length=50, default='暂无歌词')
+    song_file = models.CharField('歌曲文件', max_length=50)
+    label = models.ForeignKey(Label, on_delete=models.CASCADE,verbose_name='歌名分类')
+    def __str__(self):
+        return self.song_name
+    class Meta:
+        # 设置Admin界面的显示内容
+        verbose_name = '歌曲信息'
+        verbose_name_plural = '歌曲信息'
