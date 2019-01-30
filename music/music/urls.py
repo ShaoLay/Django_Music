d9d7e92cc506710e7ae33723966b414713bcf3b0
hamdi <javs_shao@163.com>
2019-01-30 16:34:12 +0800

    ranking_歌曲排行榜_后端全部完成

--- music/music/urls.py
+++ music/music/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
-    # path('ranking.html', include('ranking.urls')),
+    path('ranking.html', include('ranking.urls')),
     # path('play/', include('play.urls')),
     # path('comment/', include('comment.urls')),
     # path('search/', include('search.urls')),
